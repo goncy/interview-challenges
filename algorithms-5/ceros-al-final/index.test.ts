5d77f1dbe1902611c25985c9bac13a1e027341ae
goncy <gonzalo.pozzo4@gmail.com>
2022-08-16 21:56:29 -0300

    Fix tests

--- algorithms-5/ceros-al-final/index.test.ts
+++ algorithms-5/ceros-al-final/index.test.ts
@@ -4,10 +4,10 @@ import moverCeros from ".";
 
 describe("moverCeros", () => {
   it("debería mover los ceros al final", () => {
-    expect(moverCeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1])).toBe([
+    expect(moverCeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1])).toMatchObject([
       1, 2, 1, 1, 3, 1, 0, 0, 0, 0,
     ]);
-    expect(moverCeros([false, 1, 0, 1, 2, 0, 1, 3, "a"])).toBe([
+    expect(moverCeros([false, 1, 0, 1, 2, 0, 1, 3, "a"])).toMatchObject([
       false,
       1,
       1,
--- algorithms-5/palindromos/index.test.ts
+++ algorithms-5/palindromos/index.test.ts
@@ -8,9 +8,11 @@ describe("posiblePalindromo", () => {
     expect(posiblePalindromo(2121)).toBe(true);
     expect(posiblePalindromo(1331)).toBe(true);
     expect(posiblePalindromo(3357665)).toBe(true);
+    expect(posiblePalindromo(1221333)).toBe(true);
   });
 
   it("debería devolver false si no es un posible palindromo", () => {
-    expect(posiblePalindromo(1294)).toBe(true);
+    expect(posiblePalindromo(1221333555)).toBe(false);
+    expect(posiblePalindromo(1294)).toBe(false);
   });
 });
