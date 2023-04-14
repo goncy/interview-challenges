c41679ca213b7b98cdd2bfd6e67b80616f73209a
Juan Martin Rodriguez <jmartinrodriguez.work@gmail.com>
2023-04-14 13:15:03 -0300

    elementos-pares resolved

--- algorithms-4/elementos-pares/index.ts
+++ algorithms-4/elementos-pares/index.ts
@@ -1,4 +1,18 @@
 export default function filtrarPares(array: unknown[]): unknown[] {
   // TODO: implement
-  return [];
+  const mapa = new Map();
+  const result: any = [];
+  array.forEach((element) => {
+    if (!mapa.has(element)) {
+      mapa.set(element, 0);
+    }
+    mapa.set(element, mapa.get(element) + 1);
+  });
+  for (let [key, value] of mapa) {
+    if (value % 2 === 0) {
+      result.push(key);
+    }
+  }
+  console.log(result)
+  return result;
 }
