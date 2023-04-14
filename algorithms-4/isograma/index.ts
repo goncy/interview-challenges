ca976bf64c57db59eab26acbe125944eb1b15021
Juan Martin Rodriguez <jmartinrodriguez.work@gmail.com>
2023-04-14 13:55:53 -0300

    isograma completed

--- algorithms-4/isograma/index.ts
+++ algorithms-4/isograma/index.ts
@@ -1,4 +1,29 @@
 export default function esIsograma(string: string): boolean {
   // TODO: implement
-  return false;
+  const palabra = string
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[^a-z]/g, "");
+  if (palabra.split(" ").length > 1 || palabra.length === 0) {
+    return false;
+  }
+  const set = new Set();
+  for (const letra of palabra) {
+    if (set.has(letra)) {
+      return false;
+    } else {
+      set.add(letra);
+    }
+  }
+  return true;
 }
+
+/*
+MEJORA:
+export default function esIsograma(string: string): boolean {
+  // TODO: implement
+  const palabra = string.toLowerCase().normalize('NFD').replace(/[^a-z]/g, "")
+  
+  return new Set([...palabra]).size === palabra.length
+}
+*/
