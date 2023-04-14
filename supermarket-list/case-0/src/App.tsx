ee2dceea945ab6116b466140a62305c63c94d845
Juan Martin Rodriguez <jmartinrodriguez.work@gmail.com>
2023-04-14 09:45:13 -0300

    case 0 completed

--- supermarket-list/case-0/src/App.tsx
+++ supermarket-list/case-0/src/App.tsx
@@ -6,23 +6,29 @@ import styles from "./App.module.scss";
 import api from "./api";
 
 function App() {
-  const [items, setItems] = useState<Item[] | null>(null);
+  const [items, setItems] = useState<Item[]>([]);
 
   useEffect(() => {
     api.list().then(setItems);
   }, []);
 
+  const handleDelete = (id: number) => {
+    const updateItems = [...items].filter((item) => id !== item.id);
+
+    setItems(updateItems);
+  };
+
   return (
     <main className={styles.main}>
       <h1>Supermarket list</h1>
       <form>
-        <input name="text" type="text" />
+        <input autoFocus name="text" type="text" />
         <button>Add</button>
       </form>
       <ul>
         {items.map((item) => (
-          <li className={item.completed ? styles.completed : ""}>
-            {item.text} <button>[X]</button>
+          <li key={item.id} className={item.completed ? styles.completed : ""}>
+            {item.text} <button onClick={() => handleDelete(item.id)}>[X]</button>
           </li>
         ))}
       </ul>
