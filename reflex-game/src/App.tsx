3c13f10329b29306d08899f6dfdc2c474cd324ca
goncy <gonzalo.pozzo4@gmail.com>
2023-05-09 14:30:47 -0300

    Add reflex game

---
+++ reflex-game/src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <main>
+      <header>
+        <h1>{0} segundos</h1>
+      </header>
+      <section>
+        <div>Juego</div>
+      </section>
+      <footer>
+        <button>Jugar</button>
+      </footer>
+    </main>
+  );
+}
+
+export default App;
