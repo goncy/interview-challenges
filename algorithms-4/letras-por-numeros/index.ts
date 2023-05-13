c45f0ec28b899d5041166c2c4705e89187cb84ce
Juan Martin Rodriguez <jmartinrodriguez.work@gmail.com>
2023-05-12 22:11:43 -0300

    letras-por-numeros completed

--- algorithms-4/letras-por-numeros/index.ts
+++ algorithms-4/letras-por-numeros/index.ts
@@ -1,4 +1,14 @@
 export default function letrasPorNumeros(string: string): string {
   // TODO: implement
-  return "";
+  const cleanUp = string
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[^a-z]/g, "");
+  let result = "";
+  for (let i = 0; i < cleanUp.length; i++) {
+    if (cleanUp[i] === " ") continue;
+    let index = cleanUp.charCodeAt(i) - 96;
+    result += index + " ";
+  }
+  return result.trim();
 }
