3ab0813d9e1d3c0936a5297defc4b9a44da4b36b
Carlos Fernandez Cabrero <carlitostrabajo2018@outlook.es>
2023-09-30 16:13:42 +0200

    arreglo test para comprobar los valores del array

--- ejercicios-algoritmos/sesion-1/elementos-pares/index.test.ts
+++ ejercicios-algoritmos/sesion-1/elementos-pares/index.test.ts
@@ -4,8 +4,6 @@ import filtrarPares from ".";
 
 describe("filtrarPares", () => {
   it("deberia devolver solo los elementos que aparece una cantidad pares de veces", () => {
-    expect(
-      filtrarPares([1, 1, 2, 3, 4, 4, 5])
-    ).toBe([1, 4]);
+    expect(filtrarPares([1, 1, 2, 3, 4, 4, 5])).toEqual([1, 4]);
   });
 });
