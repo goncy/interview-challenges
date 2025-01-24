86b2ce8f367668be780d5d9e2aaae8cccbae41c2
Gonzalo Pozzo <gonzalo.pozzo4@gmail.com>
2025-01-24 15:34:39 -0300

    Update index.test.ts

--- ejercicios-algoritmos/sesion-4/encontrar-la-media/index.test.ts
+++ ejercicios-algoritmos/sesion-4/encontrar-la-media/index.test.ts
@@ -28,28 +28,4 @@ describe("encontrarLaMedia", () => {
 
     expect(encontrarLaMedia(actual)).toMatchObject(expected);
   });
-
-  it("debería encontrar la media correctamente - 21", () => {
-    const actual: Developer[] = [
-      {
-        firstName: "Noa",
-        lastName: "A.",
-        country: "Israel",
-        continent: "Asia",
-        age: 20,
-        language: "Ruby",
-      },
-      {
-        firstName: "Andrei",
-        lastName: "E.",
-        country: "Romania",
-        continent: "Europe",
-        age: 21,
-        language: "C",
-      },
-    ];
-    const expected = 21;
-
-    expect(encontrarLaMedia(actual)).toMatchObject(expected);
-  });
 });
